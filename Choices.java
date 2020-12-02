d1d76240a9e326c98f5df7efb4bd53e723e6dbb2
Sharps <brittanyrenahayden@gmail.com>
2020-12-02 18:23:43 -0500

    Add files via upload

---
+++ Choices.java
@@ -0,0 +1,47 @@
+package LearningJava.src;
+import java.util.Scanner;
+
+public class Choices {
+
+    private static Scanner scanner;
+    static boolean testAgain = true;
+
+    public static void main(String[] args) {
+
+        while (testAgain){
+        String question = "What is my favorite animal?";
+        String choiceOne = "Dog";
+        String choiceTwo = "Cat";
+        String choiceThree = "Bird";
+
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        System.out.println(question);
+        // Write a print statement giving the answer choices
+        System.out.println(choiceOne + " or " + choiceTwo + " or " + choiceThree);
+
+        scanner = new Scanner(System.in);
+        // Retrieve the user's input
+        String answerInput = scanner.next();
+
+
+        // If the user's input matches the correctAnswer...
+        if (answerInput.equals(correctAnswer.toLowerCase())) {
+            System.out.println("Congrats! That's the right answer.");
+            System.out.println("Thanks for taking the test.");
+            break;
+        } else {
+            System.out.println("Sorry, that's not correct. The right answer is " + correctAnswer);
+            }
+
+            if (testAgain) {
+                System.out.println("Would you like to try again? Yes/No");
+                String testAnswer = scanner.next();
+                if (testAnswer.equals("No".toLowerCase())){
+                    testAgain = false;
+                }
+            }
+        }
+    }
+}

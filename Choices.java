ca799ad09baae849b7ad342cb79f5fb5bc134297
Sharps <brittanyrenahayden@gmail.com>
2020-12-02 18:28:18 -0500

    Add additional comments

--- Choices.java
+++ Choices.java
@@ -7,7 +7,7 @@ public class Choices {
     static boolean testAgain = true;
 
     public static void main(String[] args) {
-
+        // adding while loop repeater
         while (testAgain){
         String question = "What is my favorite animal?";
         String choiceOne = "Dog";
@@ -21,25 +21,26 @@ public class Choices {
         // Write a print statement giving the answer choices
         System.out.println(choiceOne + " or " + choiceTwo + " or " + choiceThree);
 
+        // Create input var
         scanner = new Scanner(System.in);
         // Retrieve the user's input
         String answerInput = scanner.next();
 
 
         // If the user's input matches the correctAnswer...
-        if (answerInput.equals(correctAnswer.toLowerCase())) {
+        if (answerInput.equals(correctAnswer.toLowerCase())) { // .toLowerCase makes sure the answer isn't affected by upper/lowercase sensitivity
             System.out.println("Congrats! That's the right answer.");
             System.out.println("Thanks for taking the test.");
-            break;
+            break; // stop test if answer is correct
         } else {
             System.out.println("Sorry, that's not correct. The right answer is " + correctAnswer);
             }
-
+        // if boolean in the repeating while loop is true, ask to take the test again
             if (testAgain) {
                 System.out.println("Would you like to try again? Yes/No");
                 String testAnswer = scanner.next();
                 if (testAnswer.equals("No".toLowerCase())){
-                    testAgain = false;
+                    testAgain = false; // if "no" is selected, stop test from running again by changing boolean to false
                 }
             }
         }
